72ad44be196b10aa585676bc891530aff92223b5
Michael Kirby <michael.kirby032@gmail.com>
2018-01-16 09:46:26 -0800

    Add utility library for configuration things
    CR: me, myself, I

---
+++ util/config.py
@@ -0,0 +1,10 @@
+'''
+Utilities for working with configuration
+'''
+
+def configToJson(configFile):
+    '''
+    Converts a JSON configuration file to a dictionary
+    '''
+    # XXX: Need to write the json from config utility
+    return configFile
