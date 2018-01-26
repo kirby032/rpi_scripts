93c34e31c8e1286d68cb132ea097750513233659
Michael Kirby <michael.kirby032@gmail.com>
2018-01-25 23:11:42 -0800

    Add sensor test create with id
    CR: me, myself, I

--- sensors/Sensor_test.py
+++ sensors/Sensor_test.py
@@ -11,5 +11,14 @@ class TestSensor(unittest.TestCase):
         with self.assertRaises(KeyError):
             Sensor(self.EMPTY_CONFIG, 'myCallback')
 
+    def test_init_with_id(self):
+        config = '''
+        {
+            "id": "testSensor"
+        }
+        '''
+        sensor = Sensor(json.loads(config), 'myCallback')
+        self.assertEqual(sensor.id, 'testSensor')
+
 if __name__ == '__main__':
     unittest.main()
