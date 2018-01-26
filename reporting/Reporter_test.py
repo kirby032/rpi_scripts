341522cf2fa6b5fcc68e9cd4d402033f07a630aa
Michael Kirby <michael.kirby032@gmail.com>
2018-01-25 23:10:48 -0800

    Add reporter test create with id
    CR: me, myself, I

--- reporting/Reporter_test.py
+++ reporting/Reporter_test.py
@@ -11,6 +11,15 @@ class TestReporter(unittest.TestCase):
         with self.assertRaises(KeyError):
             Reporter(self.EMPTY_CONFIG)
 
+    def test_init_with_id(self):
+        config = '''
+        {
+            "id": "reporter1"
+        }
+        '''
+        reporter = Reporter(json.loads(config))
+        self.assertEqual(reporter.id, "reporter1")
+
 if __name__ == '__main__':
     unittest.main()
 
